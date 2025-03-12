ede65df1fe0ff8b579444a6d0e84462187c36dab
Ignat <ignatov_rs@abv.bg>
2025-03-12 10:44:19 +0200

    add busines accoutn to registration form

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  businesFormControlFirstPart,
+  businesFormControlSecondPart,
   registrationFormControlFirstPart,
   registrationFormControlSecondPart,
 } from "../../utils/utils";
@@ -8,50 +10,104 @@ import { FaFacebookF, FaGoogle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [isActive, setIsActive] = useState("user");
   return (
     <div className="mt-32 px-2 flex flex-col justify-center items-center">
       <div className="w-full flex flex-col justify-center items-center gap-6">
         <h1 className="text-2xl text-gray-600 font-semibold">REGISTRATION</h1>
 
-        <div className="lg:w-2/3 w-full flex flex-col lg:flex-row justify-center items-center gap-4 p-4 shadow-lg rounded-md bg-gray-100">
-          <button
-            className="flex items-center w-full sm:w-1/2 justify-center gap-3 bg-white border border-gray-300 px-4 py-2 text-base sm:text-lg text-gray-700 font-medium rounded-md transition-transform duration-300 hover:scale-105 hover:shadow-md"
-            onClick={() => console.log("Sign in with Google")}
+        <div className="lg:w-2/3 w-full flex lg:flex-row justify-center items-center gap-4 p-4 shadow-lg rounded-md bg-gray-100">
+          {/* USER ACCOUNT */}
+          <div
+            onClick={() => setIsActive("user")}
+            className={`p-4 rounded-md shadow-lg cursor-pointer ${
+              isActive === "user" ? "bg-[#12A89D]" : "bg-gray-200"
+            }`}
           >
-            <FaGoogle size={20} color="#DB4437" />
-            Sign in with Google
-          </button>
-          <button
-            className="flex items-center w-full sm:w-1/2 justify-center gap-3 bg-white border border-gray-300 px-4 py-2 text-base sm:text-lg text-gray-700 font-medium rounded-md transition-transform duration-300 hover:scale-105 hover:shadow-md"
-            onClick={() => console.log("Sign in with Facebook")}
+            <h1 className="lg:text-2xl text-lg text-white font-semibold">
+              USER ACCOUNT
+            </h1>
+          </div>
+
+          {/* BUSINESS ACCOUNT */}
+          <div
+            onClick={() => setIsActive("business")}
+            className={`p-4 rounded-md shadow-lg cursor-pointer ${
+              isActive === "business" ? "bg-[#12A89D]" : "bg-gray-200"
+            }`}
           >
-            <FaFacebookF size={20} color="#1877F2" />
-            Sign in with Facebook
-          </button>
+            <h1 className="lg:text-2xl text-lg text-white font-semibold">
+              BUSINESS ACCOUNT
+            </h1>
+          </div>
         </div>
 
-        <div className="lg:w-2/3 w-full flex lg:flex-row flex-col justify-center items-center gap-6 p-10 shadow-lg rounded-md bg-gray-100">
-          <div className="w-full flex flex-col justify-center items-center gap-6">
-            {registrationFormControlFirstPart.map((item) => (
-              <InputComponent
-                key={item.label}
-                label={item.label}
-                type={item.type}
-                placeholder={item.placeholder}
-              />
-            ))}
+        {isActive === "user" ? (
+          <>
+            <div className="lg:w-2/3 w-full flex flex-col lg:flex-row justify-center items-center gap-4 p-4 shadow-lg rounded-md bg-gray-100">
+              <button
+                className="flex items-center w-full sm:w-1/2 justify-center gap-3 bg-white border border-gray-300 px-4 py-2 text-base sm:text-lg text-gray-700 font-medium rounded-md transition-transform duration-300 hover:scale-105 hover:shadow-md"
+                onClick={() => console.log("Sign in with Google")}
+              >
+                <FaGoogle size={20} color="#DB4437" />
+                Sign in with Google
+              </button>
+              <button
+                className="flex items-center w-full sm:w-1/2 justify-center gap-3 bg-white border border-gray-300 px-4 py-2 text-base sm:text-lg text-gray-700 font-medium rounded-md transition-transform duration-300 hover:scale-105 hover:shadow-md"
+                onClick={() => console.log("Sign in with Facebook")}
+              >
+                <FaFacebookF size={20} color="#1877F2" />
+                Sign in with Facebook
+              </button>
+            </div>
+
+            <div className="lg:w-2/3 w-full flex lg:flex-row flex-col justify-center items-center gap-6 p-10 shadow-lg rounded-md bg-gray-100">
+              <div className="w-full flex flex-col justify-center items-center gap-6">
+                {registrationFormControlFirstPart.map((item) => (
+                  <InputComponent
+                    key={item.label}
+                    label={item.label}
+                    type={item.type}
+                    placeholder={item.placeholder}
+                  />
+                ))}
+              </div>
+              <div className="w-full flex flex-col justify-center items-center gap-6">
+                {registrationFormControlSecondPart.map((item) => (
+                  <InputComponent
+                    key={item.label}
+                    label={item.label}
+                    type={item.type}
+                    placeholder={item.placeholder}
+                  />
+                ))}
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="lg:w-2/3 w-full flex lg:flex-row flex-col justify-center items-center gap-6 p-10 shadow-lg rounded-md bg-gray-100">
+            <div className="w-full flex flex-col justify-center items-center gap-6">
+              {businesFormControlFirstPart.map((item) => (
+                <InputComponent
+                  key={item.label}
+                  label={item.label}
+                  type={item.type}
+                  placeholder={item.placeholder}
+                />
+              ))}
+            </div>
+            <div className="w-full flex flex-col justify-center items-center gap-6">
+              {businesFormControlSecondPart.map((item) => (
+                <InputComponent
+                  key={item.label}
+                  label={item.label}
+                  type={item.type}
+                  placeholder={item.placeholder}
+                />
+              ))}
+            </div>
           </div>
-          <div className="w-full flex flex-col justify-center items-center gap-6">
-            {registrationFormControlSecondPart.map((item) => (
-              <InputComponent
-                key={item.label}
-                label={item.label}
-                type={item.type}
-                placeholder={item.placeholder}
-              />
-            ))}
-          </div>
-        </div>
+        )}
 
         <div className="flex flex-col lg:w-2/3 lg:flex-row justify-end items-center w-full">
           <div>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -63,6 +63,68 @@ export const registrationFormControlSecondPart = [
   },
 ];
 
+export const businesFormControlFirstPart = [
+  {
+    id: "company",
+    type: "text",
+    placeholder: "Enter your company name",
+    label: "Company name",
+    componentType: "input",
+  },
+  {
+    id: "uic",
+    type: "text",
+    placeholder: "Enter your UIC",
+    label: "UIC",
+    componentType: "input",
+  },
+  {
+    id: "firstName",
+    type: "text",
+    placeholder: "Enter your first name",
+    label: "First Name",
+    componentType: "input",
+  },
+  {
+    id: "lastName",
+    type: "text",
+    placeholder: "Enter your last name",
+    label: "Last Name",
+    componentType: "input",
+  },
+];
+
+export const businesFormControlSecondPart = [
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "phone",
+    type: "text",
+    placeholder: "Enter your phone number",
+    label: "Phone Number",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+  {
+    id: "confirmPassword",
+    type: "password",
+    placeholder: "Confirm your password",
+    label: "Confirm Password",
+    componentType: "input",
+  },
+];
+
 export const topMeels = [
   {
     image:
@@ -83,8 +145,7 @@ export const topMeels = [
     title: "Lamp",
   },
   {
-    image:
-      "https://ikis-light.com/wp-content/uploads/2023/07/05-1.jpg",
+    image: "https://ikis-light.com/wp-content/uploads/2023/07/05-1.jpg",
     title: "Lamp",
   },
   {
